812c4d4148a7b9b6547a81bf55c9ef71c7ac8f4c
virusinfect <141482257+virusinfect@users.noreply.github.com>
2023-11-15 11:31:44 +0300

    a

--- its/admin.py
+++ its/admin.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
-from .models import Task, Comment,Parts,Clients,Personal,Notification
+from .models import Task, Comment,Parts,Clients,Personal,Notification,Company
 admin.site.register(Task)
 admin.site.register(Comment)
 admin.site.register(Parts)
 admin.site.register(Clients)
 admin.site.register(Personal)
 admin.site.register(Notification)
+admin.site.register(Company)
 
