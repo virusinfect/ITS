740c7aa4d31868433b64e0d134bc053132dda9f7
virusinfect <141482257+virusinfect@users.noreply.github.com>
2023-11-21 11:24:16 +0300

    a

--- sales/views.py
+++ sales/views.py
@@ -165,7 +165,7 @@ def edit_sales_ticket(request, ticket_id):
             )
 
             subject = f"{status} : {ticket.company.name} : {ticket.issue_summary} : #{ticket.ticket_id}"
-            recipient_list = [handler.email, ticket.company]
+            recipient_list = [handler.email]
             from_email = 'its-noreply@intellitech.co.ke'
 
             # Create an EmailMessage instance for HTML content
