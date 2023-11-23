ad2aaa7d45c4f80dd240ff66b392eb63ff821853
virusinfect <141482257+virusinfect@users.noreply.github.com>
2023-11-23 08:15:06 +0300

    a

--- prices/admin.py
+++ prices/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from models import Brand,LaptopPriceList,ColoursoftPriceList,FellowesPricelist
+from .models import Brand,LaptopPriceList,ColoursoftPriceList,FellowesPricelist
 # Register your models here.
 admin.site.register(Brand)
 admin.site.register(LaptopPriceList)
