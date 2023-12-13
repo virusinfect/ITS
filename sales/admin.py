7d211ce895c0edb5ee1e2f62c9b39d3c0e644d7d
virusinfect <141482257+virusinfect@users.noreply.github.com>
2023-12-13 08:22:02 +0300

    Update admin.py

--- sales/admin.py
+++ sales/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import SalesTickets,SalesTicketProducts,Orders
+from .models import SalesTickets,SalesTicketProducts,Orders,OrderProducts
 # Register your models here.
 
 admin.site.register(SalesTickets)
 admin.site.register(SalesTicketProducts)
 admin.site.register(Orders)
+admin.site.register(OrderProducts)
