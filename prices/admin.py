b5968163e10168a7fd06f4611d01ab0ab8abd0e2
virusinfect <141482257+virusinfect@users.noreply.github.com>
2023-12-13 15:51:38 +0300

    Update admin.py

--- prices/admin.py
+++ prices/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from .models import Brand,LaptopPriceList,ColoursoftPriceList,FellowesPricelist,PriceRule
+from .models import Brand,LaptopPriceList,ColoursoftPriceList,FellowesPricelist,PriceRule,Equipment
 # Register your models here.
 admin.site.register(Brand)
 admin.site.register(LaptopPriceList)
 admin.site.register(ColoursoftPriceList)
 admin.site.register(FellowesPricelist)
 admin.site.register(PriceRule)
+admin.site.register(Equipment)
